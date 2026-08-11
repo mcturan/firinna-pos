0b85211202efc52588abc061bce99a0f946a9f47
Muhammed Cevad Turan <mcturan@github.com>
2026-08-11 14:11:57 +0300

    feat: Native automatic APK download and install plugin using Android DownloadManager

--- mobile_app/android/app/src/main/java/com/firinna/posmobile/MainActivity.java
+++ mobile_app/android/app/src/main/java/com/firinna/posmobile/MainActivity.java
@@ -1,5 +1,12 @@
 package com.firinna.posmobile;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(UpdatePlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
