442a84d41dd5e3e636dc891af6d90c82483319a2
Muhammed Cevad Turan <mcturan@github.com>
2026-08-17 23:20:49 +0300

    Otomatik push - 17.08.2026 23:20

--- app.py
+++ app.py
@@ -54,8 +54,8 @@ def api_mobile_version():
     return jsonify({
         'version': APP_VERSION,
         'apk_url': '/download_apk',
-        'tv_version': '1.1.11',
-        'tv_apk_url': '/static/Firinna-TV-1.1.11.apk'
+        'tv_version': '1.1.12',
+        'tv_apk_url': '/static/Firinna-TV-1.1.12.apk'
     })
 
 @app.route('/download_apk')
